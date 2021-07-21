81dd85cb5e605c6778e71b36738a8bef9b6182a6
Elliott Botwick <elliott.botwick@dominodatalab.com>
2021-07-21 15:43:09 -0700

    update test_model.sh to reflect predict.py updates
    Pushed from Domino: /u/elliott_botwick/Model-Pipeline-GBP/runs/60f82d8ca853091aa99a7a7d

--- test_model.sh
+++ test_model.sh
@@ -7,7 +7,7 @@ AUTH_CREDENTIALS="uYZUb5q10eNfLmnXepFdW1MscL9kKTX5wK5bf2vrnhw9xjMql236UNnMBBa1Cc
 
 MODEL_ID=$1
 
-TEST=$(curl ${DOMINO_HOST}/models/${MODEL_ID}/latest/model -s -H 'Content-Type: application/json' -d '{ "data": { "dropperc": 1000, "mins": 941, "consecmonths": 29, "income": 35000 } }' -u $AUTH_CREDENTIALS | grep "request_id" | wc -l)
+TEST=$(curl ${DOMINO_HOST}/models/${MODEL_ID}/latest/model -s -H 'Content-Type: application/json' -d '{ "data": { "dropperc": 1000, "mins": 941, "consecmonths": 29, "income": 35000, "age": 45 } }' -u $AUTH_CREDENTIALS | grep "request_id" | wc -l)
 
 if [[ $TEST -gt 0 ]]; then
     echo "TEST: PASS"
